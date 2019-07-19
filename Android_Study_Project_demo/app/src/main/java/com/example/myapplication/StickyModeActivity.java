060a5dc850a0c10620f094d2cd5b5605ff7b2b6a
DongLy <448189597@qq.com>
2019-07-19 16:50:53 +0800

    sticky demo

---
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/StickyModeActivity.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+/*
+ * @Author: Luke
+ * @Date: 2019-07-19 16:06
+ * @Sign: Cherish life and keep away from bugs!
+ * @Project: Android_Study_Project_demo
+ */public class StickyModeActivity {
+}
---
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/TestActivity.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/*
+ * @Author: Luke
+ * @Date: 2019-07-19 16:15
+ * @Sign: Cherish life and keep away from bugs!
+ * @Project: Android_Study_Project_demo
+ */public class TestActivity extends AppCompatActivity {
+}
