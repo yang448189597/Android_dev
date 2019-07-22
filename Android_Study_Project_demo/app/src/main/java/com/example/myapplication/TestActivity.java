9c3d4243913d126a42eae423492bcd109f907d11
DongLy <448189597@qq.com>
2019-07-22 10:53:40 +0800

    add butterknife lib

--- Android_Study_Project_demo/app/src/main/java/com/example/myapplication/TestActivity.java
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/TestActivity.java
@@ -1,9 +1,15 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /*
  * @Author: Luke
@@ -13,9 +19,34 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class TestActivity extends AppCompatActivity {
 
+    @BindView(R.id.btn_1)
+    Button btn1;
+    @BindView(R.id.btn_2)
+    Button btn2;
+    @BindView(R.id.btn_3)
+    Button btn3;
+    @BindView(R.id.btn_4)
+    Button btn4;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_test);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_1:
+                Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btn_2:
+                break;
+            case R.id.btn_3:
+                break;
+            case R.id.btn_4:
+                break;
+        }
     }
 }
