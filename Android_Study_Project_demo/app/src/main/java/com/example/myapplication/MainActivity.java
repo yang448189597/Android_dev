532a621176f86272154a2d4cbd87916b28478ea8
DongLy <448189597@qq.com>
2019-07-26 16:29:19 +0800

    add RxPermissions demo / implementation rxandroid/java

--- Android_Study_Project_demo/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermission();
+    }
+
+    private void requestPermission() {
+        final RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.CAMERA).subscribe(granted -> {
+                    if (granted) { // Always true pre-M
+                        // I can control the camera now
+                        Toast.makeText(this,"open camera",Toast.LENGTH_LONG).show();
+                    } else {
+                        // Oups permission denied
+                        Toast.makeText(this,"no camera",Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
