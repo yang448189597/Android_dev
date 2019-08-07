f59768c8fd2d1e4058e588c4155b369ab2890b54
DongLy <448189597@qq.com>
2019-08-07 17:16:36 +0800

    add BaseRecyclerViewAdapterHelper lib

---
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/view/RecyclerView/entity/MultiItemEntity.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.view.RecyclerView.entity;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface MultiItemEntity {
+    int getItemType();
+}
