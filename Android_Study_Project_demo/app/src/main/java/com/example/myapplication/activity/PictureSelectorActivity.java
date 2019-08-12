8ff49236ecb9adc4ab1471ff42141a6f17d8a485
DongLy <448189597@qq.com>
2019-08-12 20:21:51 +0800

    add picture selector

---
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/activity/PictureSelectorActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication.activity;
+
+import com.example.myapplication.common.BaseActivity;
+
+/*
+ * @Author: Luke
+ * @Date: 2019-08-12 20:19
+ * @Sign: Cherish life and keep away from bugs!
+ * @Project: Android_Study_Project_demo
+ */
+public class PictureSelectorActiviity extends BaseActivity {
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public int getContentViewResId() {
+        return 0;
+    }
+}
