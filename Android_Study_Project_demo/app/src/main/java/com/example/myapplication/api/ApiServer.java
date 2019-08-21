4d61f0c6bd6da90635a0b3226b2cc2b678e90df5
DongLy <448189597@qq.com>
2019-08-21 20:58:43 +0800

    add api server

---
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/api/ApiServer.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.api;
+
+/*
+ * @Author: Luke
+ * @Date: 2019-08-21 15:42
+ * @Sign: Cherish life and keep away from bugs!
+ * @Project: Android_Study_Project_demo
+ */
+public class ApiServer {
+
+    private String IMAGE_API = "";
+}
