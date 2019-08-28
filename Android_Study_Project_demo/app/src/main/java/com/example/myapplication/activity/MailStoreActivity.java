188d045f1641ea43daacf6873e73fef6b3ceb5a8
DongLy <448189597@qq.com>
2019-08-28 20:24:51 +0800

    some changed

---
+++ Android_Study_Project_demo/app/src/main/java/com/example/myapplication/activity/MailStoreActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.activity;
+
+/*
+ * @Author: Luke
+ * @Date: 2019-08-28 20:24
+ * @Sign: Cherish life and keep away from bugs!
+ * @Project: Android_Study_Project_demo
+ */
+public class MailStoreActivity {
+}
