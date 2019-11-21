244a81c6342e7c5d4e81cf43fd2d271800db951d
DongLy <448189597@qq.com>
2019-11-21 14:35:44 +0800

    completed Socre

--- Socre/app/src/main/java/com/example/socre/MainActivity.java
+++ Socre/app/src/main/java/com/example/socre/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.socre;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import com.example.socre.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    MyViewModel myViewModel;
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
+
     }
 }
---
+++ Socre/app/src/main/java/com/example/socre/MyViewModel.java
@@ -0,0 +1,58 @@
+package com.example.socre;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+/*
+ * @Author: Luke
+ * @Date: 2019-11-21 12:28
+ * @Sign: Cherish life and keep away from bugs!
+ * @Project: Socre
+ */
+public class MyViewModel extends ViewModel {
+    private MutableLiveData<Integer> aTeamScore;
+    private MutableLiveData<Integer> bTeamScore;
+    private int aBack, bBack;
+
+    public MutableLiveData<Integer> getaTeamScore() {
+        if (aTeamScore == null) {
+            aTeamScore = new MutableLiveData<>();
+            aTeamScore.setValue(0);
+        }
+        return aTeamScore;
+    }
+
+    public MutableLiveData<Integer> getbTeamScore() {
+        if (bTeamScore == null) {
+            bTeamScore = new MutableLiveData<>();
+            bTeamScore.setValue(0);
+        }
+        return bTeamScore;
+    }
+
+    public void aTeamAdd(int p) {
+        aBack = aTeamScore.getValue();
+        bBack = bTeamScore.getValue();
+        aTeamScore.setValue(aTeamScore.getValue() + p);
+
+    }
+
+    public void bTeamAdd(int p) {
+        aBack = aTeamScore.getValue();
+        bBack = bTeamScore.getValue();
+        bTeamScore.setValue(bTeamScore.getValue() + p);
+    }
+
+    public void reset() {
+        aBack = aTeamScore.getValue();
+        bBack = aTeamScore.getValue();
+        aTeamScore.setValue(0);
+        bTeamScore.setValue(0);
+    }
+
+    public void undo() {
+        aTeamScore.setValue(aBack);
+        bTeamScore.setValue(bBack);
+    }
+}
